17ec65abf5a3b23014bb7390d9938fede28dcf33
Nicholas Zhaosi <weihl18@lzu.edu.cn>
2020-05-30 18:13:50 +0800

    健康打卡脚本

--- four order Matrix/kagome_ploat.py
+++ four order Matrix/kagome_ploat.py
@@ -3,22 +3,31 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
-import ForMMA as mma
+from ForMMA import *
+
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection = '3d')
-ky = np.arange(-4, 4, 0.25)
-kx = np.arange(-4, 4, 0.25)
+ky = np.arange(-4, 4, 0.5)
+kx = np.arange(-4, 4, 0.5)
 # 构造kxky网格平面
 kx,ky=np.meshgrid(kx,ky)
 
 ##压平kx和ky。
 kkx=kx.flatten()
 kky=ky.flatten()
-XY=mma.partition(mma.riffle(kkx,kky),2)
+XY=mma.partition(mma.riffle(kkx,kky),2) 
 ##获取本征值
 Z=mma.map(Kagome_Eigvalues,XY)
 ##取第各列
+for j in range(0,4):
+	ZZ=np.array(Z)[:,j]
+	##将它重新组织成meshgrid应该对应的形式
+	ZZZ=np.array(mma.partition(ZZ,len(kx)))
+	fig=plt.figure()
+	ax=Axes3D(fig)
+	ax.scatter(kx.flatten().tolist(),ky.flatten().tolist(),ZZZ.flatten().tolist())
+"""
 ZZ=[]
 for i in range(0,len(np.array(Z)[0])) :
 	ZZ.append(np.array(Z)[:,i])
@@ -27,6 +36,8 @@ ZZZ=[]
 for i in range(0,len(ZZ)) :
 	ZZZ.append(np.array(mma.partition(ZZ[i],len(kx))))
 
+ZZ
+"""
 
 
 # Plot the surface.
---
+++ 自动打卡/python自动打卡.py
@@ -0,0 +1,61 @@
+# 导入包
+'''
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pyautogui as pag
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+'''
+# 登录
+driver = webdriver.Edge('G:\\Edge浏览器下载\\edgedriver_win64\\msedgedriver.exe')
+driver.get("http://my.lzu.edu.cn:8080/login?service=http://my.lzu.edu.cn")
+name = driver.find_element(By.NAME, 'username')
+name.clear()
+name.send_keys('weihl18')
+password = driver.find_element(By.NAME, 'password')
+password.clear()
+password.send_keys('wei199903')
+button_login = driver.find_element_by_xpath(
+    '//*[@id="loginForm"]/div[3]/button')
+button_login.click()
+
+# 打开健康打卡小程序
+button_app = driver.find_element_by_xpath('//*[@id="my-apps"]/li[5]')
+ActionChains(driver).click(button_app).perform()
+
+# 切换到frame中
+driver.switch_to_frame('iframe')
+
+# 判断是否已打卡
+time.sleep(2)
+xpath_of_concern = '/html/body/uni-app/uni-modal/div[2]/div[3]/div'
+try:
+    try:
+        driver.find_element_by_xpath(
+            xpath_of_concern).get_attribute('innerText') == '确定'
+        print('已打卡')
+        driver.find_element_by_xpath(xpath_of_concern).click()
+    except:
+        print('未打卡')
+        tempretrue='/html/body/uni-app/div/div[2]/uni-picker-view/div/uni-picker-view-column[1]/div/div[3]/div[4]'
+        concern_tempretrue='/html/body/uni-app/div/div[2]/div/div[2]'
+        tempretrue_selector='/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[3]/uni-view/uni-form/span/uni-view[8]/uni-view/uni-picker/div[2]/uni-view'
+
+        driver.find_element_by_xpath(tempretrue_selector).click()
+        driver.find_element_by_xpath(tempretrue).click()
+        driver.find_element_by_xpath(concern_tempretrue).click()
+except:
+    print('过程失败')
+
+tempretrue='/html/body/uni-app/div/div[2]/uni-picker-view/div/uni-picker-view-column[1]/div/div[3]/div[4]'
+concern_tempretrue='/html/body/uni-app/div/div[2]/div/div[2]'
+tempretrue_selector='/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[3]/uni-view/uni-form/span/uni-view[8]/uni-view/uni-picker/div[2]/uni-view'
+xpath_of_commit='/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[3]/uni-view/uni-form/span/uni-view[12]/uni-button'
+
+driver.find_element_by_xpath(tempretrue_selector).click()
+time.sleep(2)
+driver.find_element_by_xpath(tempretrue).click()
+driver.find_element_by_xpath(concern_tempretrue).click()
+driver.find_element_by_xpath(xpath_of_commit).click()
+print('执行完毕')
